dc1e203749235caec83b9b540f755274f8121a73
Saurav Chhatrapati <sauravc@berkeley.edu>
2018-06-21 15:28:47 -0700

    Removed sky specific code

--- client.py
+++ client.py
@@ -1,81 +1,18 @@
 import cloudpickle as cp
-import boto3
 import requests
-import utils
 
 class SkyConnection():
-    def __init__(self, addr='127.0.0.1', port=7000, pr_type='FS', pr_info={'dir': 'funcs/'}):
+    def __init__(self, addr='127.0.0.1', port=7000):
         self.service_addr = addr + ":" + str(port)
-
-        pr_dict = { 'type': pr_type, 'info': pr_info }
-        self.provider = utils.create_provider(pr_type, pr_info)
-
         self.session = requests.Session()
-        r = self.session.post(self.service_addr + "/connect", data=cp.dumps(pr_dict))
-
-    def list(self, prefix=None):
-        for fname in self._get_func_list(prefix):
-            print(fname)
-
-    def get(self, name):
-        if name not in self._get_func_list():
-            print("No function found with name '" + name + "'.")
-            print("To view all functions, use the `list` method.")
-            return None
-
-        return SkyFunc(name, self._name_to_handle(name), self)
-
-    def _get_func_list(self, prefix=None):
-        if prefix == None:
-            r = self.session.get(self.service_addr + "/list")
-        else:
-            r = self.session.get(self.service_addr + "/list/" + prefix)
-        return cp.loads(r.content)
-
-
-    def _name_to_handle(self, name):
-        return name
 
     def exec_func(self, handle, args):
-        args_bin = cp.dumps(args)
+        data = [handle, args]
+        data_bin = cp.dumps(data)
 
-        r = self.session.post(self.service_addr + "/" + handle, data=args_bin)
+        r = self.session.post(self.service_addr + "/execute", data=data_bin)
         return cp.loads(r.content)
 
-    def register(self, func, name):
-        self.session.post(self.service_addr + "/create/" + name, data=cp.dumps(func))
-
-    def deregister(self, name):
-        self.session.post(self.service_addr + "/remove/" + name)
-
-
-class SkyFuture():
-    def __init__(self, obj_id, conn):
-        self.obj_id = obj_id
-        self.conn = conn
-
-    def get(self):
-        obj = utils.get_provider(self.conn.provider).get(self.obj_id)
-
-        while not obj:
-            obj = utils.get_provider(self.conn.provider).get(self.obj_id)
-
-        return cp.loads(obj)
-
-class SkyFunc():
-    def __init__(self, name, func_handle, conn):
-        self.name = name
-        self.handle = func_handle
-        self._conn = conn
-
-    def __call__(self, *args):
-        obj_id = self._conn.exec_func(self.handle, args)
-        return SkyFuture(obj_id, self._conn)
-
-class SkyReference():
-    def __init__(self, key, deserialize):
-        self.key = key
-        self.deserialize = deserialize
 
 def connect(addr=None, port=7000):
     return SkyConnection(addr, port)
--- server.py
+++ server.py
@@ -1,88 +1,22 @@
-import boto3
 import cloudpickle as cp
 import flask
 from flask import session
 from flask_session import Session
-from threading import Thread
-import uuid
-import utils
 
 app = flask.Flask(__name__)
 
 @app.route('/connect', methods=['POST'])
 def connect():
-    pr_data = cp.loads(flask.request.get_data())
-    pr_type = pr_data['type']
-    info = pr_data['info']
-
-    flask.session['provider'] = utils.create_provider(pr_type, info)
     return construct_response()
 
-@app.route('/create/<funcname>', methods=['POST'])
-def create_func(funcname):
-    provider = utils.get_provider(flask.session['provider'])
-    func_binary = flask.request.get_data()
-
-    app.logger.info('Creating function: ' + funcname + '.')
-    provider.put(funcname, func_binary)
-
-    return construct_response()
-
-
-@app.route('/remove/<funcname>', methods=['POST'])
-def remove_func(funcname):
-    provider = utils.get_provider(flask.session['provider'])
-
-    app.logger.info('Removing function: ' + funcname + '.')
-    provider.remove(funcname)
+@app.route('/execute', methods=['POST'])
+def execute():
+    data_bin = flask.request.get_data()
+    data = cp.loads(data_bin)
+    funcname = data[0]
+    args = data[1]
 
     return construct_response()
-
-
-@app.route('/<funcname>', methods=['POST'])
-def call_func(funcname):
-    obj_id = str(uuid.uuid4())
-    t = Thread(target = _exec_func, args = (funcname, flask.session['provider'], obj_id, flask.request.get_data()))
-    t.start()
-
-    return construct_response(obj_id)
-
-
-def _exec_func(funcname, p_obj, obj_id, arg_obj):
-    provider = utils.get_provider(p_obj)
-    func_binary = provider.get(funcname)
-    func = cp.loads(func_binary)
-
-    args = cp.loads(arg_obj)
-
-    func_args = ()
-
-    for arg in args:
-        if isinstance(arg, sky.SkyReference):
-            func_args = (_resolve_ref(arg, provider),)
-        else:
-            func_args += (arg,)
-
-
-    res = func(*args)
-
-    provider.put(obj_id, cp.dumps(res))
-
-def _resolve_ref(ref, provider):
-    ref_data = provider.get_object(ref.key)
-
-    if ref.deserialize:
-        return cp.loads(ref_data)
-    else:
-        return ref_data
-
-@app.route('/list', methods=['GET'])
-@app.route('/list/<prefix>', methods=['GET'])
-def list_funcs(prefix=''):
-    provider = utils.get_provider(flask.session['provider'])
-    result = provider.get_list(prefix)
-
-    return construct_response(result)
 
 def construct_response(obj=None):
     resp = flask.make_response()
