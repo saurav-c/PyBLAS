5f9bedda0c2b96227188a2ba7cdd2604d91c7617
Saurav Chhatrapati <sauravc@berkeley.edu>
2018-06-26 14:57:33 -0700

    Added more vector funcs

--- utils.cpp
+++ utils.cpp
@@ -9,7 +9,11 @@ BOOST_PYTHON_MODULE(utils)
 {
     class_<vector>("Vector")
     	.def(init<unsigned int>())
+    	.def(init<vector>())
     	.def("size", &vector::size)
+    	.def("resize", &vector::resize)
+    	.def("max_size", &vector::max_size)
+    	.def("empty", &vector::empty)
     ;
 }
 
