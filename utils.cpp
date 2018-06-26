1233b3becc532f52317e4257ece45a75ffdebd9a
Saurav Chhatrapati <sauravc@berkeley.edu>
2018-06-26 15:49:07 -0700

    Fixed bugs

--- utils.cpp
+++ utils.cpp
@@ -6,6 +6,7 @@ using namespace boost::python;
 typedef boost::numeric::ublas::vector<double> BaseVector;
 
 class Vector : public BaseVector {
+public:
 	void set(unsigned int index, double value) {
 		this(index) = value;
 	}
@@ -13,13 +14,13 @@ class Vector : public BaseVector {
 	double get(unsigned int index) {
 		return this(index);
 	}
-} 
+}; 
 
 BOOST_PYTHON_MODULE(utils)
 {
     class_<Vector>("Vector")
     	.def(init<unsigned int>())
-    	.def(init<vector>())
+    	.def(init<Vector>())
     	.def("size", &Vector::size)
     	.def("resize", &Vector::resize)
     	.def("max_size", &Vector::max_size)
