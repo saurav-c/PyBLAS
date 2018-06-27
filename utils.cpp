13d549ce2c67f88fd578d6b62457dfaede497ff9
Saurav Chhatrapati <sauravc@berkeley.edu>
2018-06-27 11:36:49 -0700

    Added vector indexing

--- utils.cpp
+++ utils.cpp
@@ -1,13 +1,10 @@
 #include <boost/python.hpp>
 #include <boost/numeric/ublas/vector.hpp>
+#include <boost/python/suite/indexing/vector_indexing_suite.hpp>
 
 using namespace boost::python;
 
 typedef boost::numeric::ublas::vector<double> Vector;
-
-double get(Vector vector, unsigned int index) {
-	return vector(index)
-}
 
 BOOST_PYTHON_MODULE(utils)
 {
@@ -18,9 +15,15 @@ BOOST_PYTHON_MODULE(utils)
     	.def("resize", &Vector::resize)
     	.def("max_size", &Vector::max_size)
     	.def("empty", &Vector::empty)
-    	.def("__getitem__", &Vector::operator[], arg("index"),
-    	return_internal_reference<>())
-    	
+    	.def("swap", &Vector::swap)
+    	.def("insert_element", &Vector::insert_element)
+    	.def("erase_element", &Vector::erase_element)
+    	.def("clear", &Vector::clear)
+    	.def("begin", &Vector::begin)
+    	.def("end", &Vector::end)
+    	.def("rbegin", &Vector::rbegin)
+    	.def("rend", &Vector::rend)
+    	.def(vector_indexing_suite<std::vector<double>>())
     ;
 }
 
