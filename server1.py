48924d821a08699b2c11fbb5f70aba2798100c88
Saurav Chhatrapati <sauravc@berkeley.edu>
2018-07-03 12:18:36 -0700

    More debugging

--- server1.py
+++ server1.py
@@ -25,10 +25,10 @@ def connect():
 @app.route('/vector', methods=['POST'])
 def vector():
     args = cp.loads(flask.request.get_data())
-    vec = pyblas.Vector(args)
+    #vec = pyblas.Vector(args)
 
     key = flask.session['OBJ_ID']
-    flask.session['dict'][key] = vec
+    flask.session['dict'][key] = 'vec'
 
     resp = key
     flask.session['OBJ_ID'] += 1
